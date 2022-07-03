f96090776cb992e3f9ebfc21e23e5b9f86d13252
Harshbir21102043 <104373657+Harshbir21102043@users.noreply.github.com>
2022-07-03 16:14:02 +0530

    Add files via upload

---
+++ 6) output.py
@@ -0,0 +1,67 @@
+Python 3.10.1 (tags/v3.10.1:2cd268a, Dec  6 2021, 19:10:37) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+== RESTART: C:\Users\chann\AppData\Local\Programs\Python\Python310\6) 1st q.py =
+Enter a number: 6
+6  is a perfect number
+
+
+== RESTART: C:\Users\chann\AppData\Local\Programs\Python\Python310\6) 2nd q.py =
+Enter a String: racecar
+racecar is a palindrome
+
+
+== RESTART: C:\Users\chann\AppData\Local\Programs\Python\Python310\6) 3rd q.py =
+Enter a number: 5
+      1 
+     1 1 
+    1 2 1 
+   1 3 3 1 
+  1 4 6 4 1 
+
+
+== RESTART: C:\Users\chann\AppData\Local\Programs\Python\Python310\6) 4th q.py =
+Enter a String: the quick brown fox jumps over a lazy hazy dazy dog
+Yes 'the quick brown fox jumps over a lazy hazy dazy dog' is a panagram
+
+
+== RESTART: C:\Users\chann\AppData\Local\Programs\Python\Python310\6) 5th q.py =
+Enter a hyphen separated sentence: red-yellow-blue-green
+blue-green-red-yellow
+
+
+== RESTART: C:\Users\chann\AppData\Local\Programs\Python\Python310\6) 6th q.py =
+Student ID= 21102043
+Student ID= 21102043
+Student Name= Harshbir Singh
+Student ID= 21102043
+Student Name= Harshbir Singh
+Student Class= 1st Year
+
+
+== RESTART: C:/Users/chann/AppData/Local/Programs/Python/Python310/6) 7th q.py =
+True
+False
+True
+False
+
+Check whether the said classes are subclasses of the built-in object class or not.
+True
+True
+
+
+== RESTART: C:/Users/chann/AppData/Local/Programs/Python/Python310/6) 8th q.py =
+Enter number of elements : 6
+-4
+3
+1
+5
+6
+-11
+-4 3 1
+5 6 -11
+None
+
+== RESTART: C:/Users/chann/AppData/Local/Programs/Python/Python310/6) 9th q.py =
+Enter the sequence of parantheses : {}[]
+{}[] - is balanced
